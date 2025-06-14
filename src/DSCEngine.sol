d8e36673181189fbfdb0d9026f5c19a0013de5c7
okweb-3 <fortwu06@gmail.com>
2025-06-14 16:01:57 +0800

    Finish Creating the mint function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -42,6 +42,7 @@ pragma solidity ^0.8.18;
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract DSCEngine is ReentrancyGuard {
     /**
@@ -78,7 +79,12 @@ contract DSCEngine is ReentrancyGuard {
     DecentralizedStableCoin private immutable i_dsc;
     //keep track of collateral deposited by each user. 
     mapping (address user => mapping(address token => uint256 amount))private s_collateralDeposited;
+    //reflect the amount being minted in our function
+    mapping (address user => uint amountDscMinted) private s_DSCMinted;
+    address[] private s_collateralTokens;
 
+    uint256 private constant ADDITIONAL_FEED_PRECISION=1e10;
+    uint256 private constant PRECISION = 1e18;
      /*//////////////////////////////////////////////////////////////
                                  EVENTS
     //////////////////////////////////////////////////////////////*/
@@ -105,6 +111,9 @@ contract DSCEngine is ReentrancyGuard {
     /*//////////////////////////////////////////////////////////////
                                FUNCTIONS
     //////////////////////////////////////////////////////////////*/
+
+    
+
     //Initialize this mapping in our contract's constructor 
     constructor(address[] memory tokenAddress,address[] memory priceFeedAddress,address dscAddress){
         if(tokenAddress.length != priceFeedAddress.length){
@@ -112,6 +121,7 @@ contract DSCEngine is ReentrancyGuard {
         }
         for(uint256 i=0;i<tokenAddress.length;i++){
             s_priceFeeds[tokenAddress[i]=priceFeedAddress[i]];
+            s_collateralTokens.push(tokenAddress[i]);
         }
         i_dsc = DecentralizedStableCoin(dscAddress);
     }
@@ -130,7 +140,6 @@ contract DSCEngine is ReentrancyGuard {
         bool success =  IERC20(tokenCollateralAddress).transferFrom(msg.sender,address(this),amountCollateral);
         if (!success){
             revert DSCEngine__TransferFailed();
-
         }
     }
 
@@ -146,5 +155,35 @@ contract DSCEngine is ReentrancyGuard {
 
     function getHeathFactor() external view{}
 
+    function mintDsc(uint256 amountDscToMint) public moreThanZero(amountDscToMint) nonReentrant{
+        s_DSCMinted[msg.sender] +=amountDscToMint;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                      PRIVATEINTERNALVIEWFUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function _revertIfHealthFactorIsBroken(address user)internal view{}
+
+    function _healthFactor(address user) private view returns(uint256){
+        (uint256 totalDscMinted, uint256 collateralValueInUsd) = _getAccountInformation(user);
+    }
+
+    function _getAccountInformation(address user)private view returns(uint256 totalDscMinted, uint256 collateralValueInusd){
+        totalDscMinted = s_DSCMinted[user];
+        collateralValueInusd = getAccountCollateralValue(user);
+    }
+    function getAccountCollateralValue(address user) public  view returns(uint256 totalCollateralValueInUsd){
+        for(uint256 i = 0; i<s_collateralTokens.length;i++){
+            address token = s_collateralTokens[i];
+            uint256 amount = s_collateralDeposited[user][token];
+            totalCollateralValueInUsd += getUsdValue(token,amount);
+        }
+        return totalCollateralValueInUsd;
+    }
+    function getUsdValue(address token, uint256 amount) public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
+        (,int256 price,,,)=priceFeed.latestRoundData();
+        return ((uint256(price)*ADDITIONAL_FEED_PRECISION)*amount)/PRECISION;
+    }
     
 }
