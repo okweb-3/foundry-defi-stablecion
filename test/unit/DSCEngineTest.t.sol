da61d773425158e4c199c5e5a1492107884dfe40
okweb-3 <fortwu06@gmail.com>
2025-06-19 23:16:23 +0800

    Add some Test

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -3,51 +3,89 @@ pragma solidity ^0.8.18;
 
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
-import {DecentralizedStableCoin}  from "../../src/DecentralizedStableCoin.sol";
-import {Test,console} from "lib/forge-std/src/Test.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {Test, console} from "lib/forge-std/src/Test.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
+contract DSCEngineTest is Test {
+    DeployDSC deployer;
+    DecentralizedStableCoin dsc;
+    DSCEngine dsce;
+    HelperConfig helperConfig;
+    address ethUsdPriceFeed;
+    address btcUsdPriceFeed;
+    address weth;
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
 
-contract DSCEngineTest is Test{
-        DeployDSC deployer;
-        DecentralizedStableCoin dsc;
-        DSCEngine dsce;
-        HelperConfig  helperConfig;
-        address ethUsdPriceFeed;
-        address weth;
-        address public USER = makeAddr("user");
-        uint256 public constant AMOUNT_COLLATERAL = 10 ether;
-        uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+    function setUp() public {
+        deployer = new DeployDSC();
+        (dsc, dsce, helperConfig) = deployer.run();
+        (ethUsdPriceFeed, btcUsdPriceFeed, weth, , ) = helperConfig
+            .activeNetworkConfig();
 
-        function setUp() public{
-            deployer = new DeployDSC();
-            (dsc,dsce,helperConfig) = deployer.run();
-            (ethUsdPriceFeed,, weth, ,) = helperConfig.activeNetworkConfig();
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+    }
 
-            ERC20Mock(weth).mint(USER,STARTING_ERC20_BALANCE);
-        }
-        /*//////////////////////////////////////////////////////////////
+    ////////////////////////////
+    /// Constructor Test   /////
+    ////////////////////////////
+    address[] public tokenAddresses;
+    address[] public priceFeedAddress;
+    function testRevertIfTokenLengthDoesntMatchPrriceFeeds() public {
+        tokenAddresses.push(weth);
+        priceFeedAddress.push(ethUsdPriceFeed);
+        priceFeedAddress.push(btcUsdPriceFeed);
+        vm.expectRevert(
+            DSCEngine
+                .DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength
+                .selector
+        );
+        new DSCEngine(tokenAddresses, priceFeedAddress, address(dsc));
+    }
+
+    ////////////////////////////
+    ////// Price  Test  ///////
+    ////////////////////////////
+
+    function testgetTokenAmountFromUsd() public view {
+        uint256 usdAmount = 100 ether;
+        uint256 expectedWeth = 0.05 ether;
+        uint256 actualWeth = dsce.getTokenAmountFromUsd(weth, usdAmount);
+        assertEq(expectedWeth, actualWeth);
+    }
+
+    /*//////////////////////////////////////////////////////////////
                                PRICE_TEST
         //////////////////////////////////////////////////////////////*/
-        function testGetUsdValue() view public{
-            //15e18*2000/ETH = 30000e18;
-            uint256 ethAmount = 15e18;
-            uint256 expectedUsd = 30000e18;
-            uint256 actualUsd = dsce.getUsdValue(weth,ethAmount);
-            console.log("expectedUsd: ",expectedUsd );
-            console.log("actualUsd: ",actualUsd );
-            assertEq(expectedUsd,actualUsd);
-        }
-        /*//////////////////////////////////////////////////////////////
+    function testGetUsdValue() public view {
+        //15e18*2000/ETH = 30000e18;
+        uint256 ethAmount = 15e18;
+        uint256 expectedUsd = 30000e18;
+        uint256 actualUsd = dsce.getUsdValue(weth, ethAmount);
+        console.log("expectedUsd: ", expectedUsd);
+        console.log("actualUsd: ", actualUsd);
+        assertEq(expectedUsd, actualUsd);
+    }
+    /*//////////////////////////////////////////////////////////////
                         DEPOSITCOLLATERAL_TESTS
         //////////////////////////////////////////////////////////////*/
-        function testRevertsIfCollateralZero()public{
-            vm.startPrank(USER);
-            ERC20Mock(weth).approve(address(dsce),AMOUNT_COLLATERAL);
+    function testRevertsIfCollateralZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
 
-            vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
-            dsce.depositCollateral(weth,0);
-            vm.stopPrank();
-        }
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
+
+    function testRevertsWithUnapprovedCollateral() public {
+        ERC20Mock ranToken = new ERC20Mock();
+        vm.startPrank(USER);
+        vm.expectRevert(DSCEngine.DSCEngine__TokenNotAllowed.selector);
+        dsce.depositCollateral(address(ranToken), AMOUNT_COLLATERAL);
+        vm.stopPrank();
+    }
 }
